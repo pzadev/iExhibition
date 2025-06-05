a6ae73c2e4c0ddbb5c96864fa19e65deb5698da5
Peter Zak Austin <peterzaustin@icloud.com>
2025-06-05 12:10:53 +0100

    mobile UI qol fix

--- src/Bar.tsx
+++ src/Bar.tsx
@@ -6,24 +6,33 @@ function Bar() {
 
   return (
     <header className="w-full bg-blue-800 border-b-1 border-gray-400 shadow px-4 py-5 flex items-center justify-between">
-          <div
+      <div
         onClick={() => navigate("/")}
         className="flex items-center gap-2 cursor-pointer"
       >
-
-        <img src="/museumIcon.png" alt="Museum Icon" className="h-11 w-11 mr-1" />
-       <span className="text-3xl font-bold text-white">iExhibition</span>
+        <img
+          src="/museumIcon.png"
+          alt="Museum Icon"
+          className="h-11 w-11 mr-1"
+        />
+        <span className="text-3xl font-bold text-white">iExhibition</span>
       </div>
 
       <div className="flex gap-4 items-center">
         <button
           onClick={() => navigate("/exhibition")}
-          className={`text-lg font-semibold text-white hover:text-gray-300 transition-colors px-4 py-2 rounded-full border-2 cursor-pointer
+          className={`
+    text-base sm:text-lg font-semibold
+    text-white hover:text-gray-300 transition-colors
+    px-3 py-1 sm:px-4 sm:py-2
+    rounded-full border border-white
+    cursor-pointer
     ${
       location.pathname === "/exhibition"
         ? "text-white"
-        : "text-gray-600 hover:text-blue-600"
-    }`}
+        : "text-gray-300 hover:text-blue-300"
+    }
+  `}
         >
           Your Exhibitions
         </button>
